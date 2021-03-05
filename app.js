1f85e209852a5f1dba5bd15b02f176a5b3e101b6
MuzaffarMohammed-Tuple <79186924+MuzaffarMohammed-Tuple@users.noreply.github.com>
2021-03-05 22:48:21 +0530

    mail response OK text addition

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.post('/send', (req, res) => {
       console.log('Message sent: %s', info.messageId);   
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-      res.render('OK');
+      res.send("OK");
   });
   });
 
